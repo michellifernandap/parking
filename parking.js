66cd7adec63b2125f68f492b539cf8f39dd198f1
Michelli Prieto <Michellifernanda.fp@gmail.com>
2024-02-04 11:48:58 +0100

    ok

---
+++ parking.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function changeBoxColors() {
+        var colorBoxes = document.querySelectorAll('.color-box');
+        colorBoxes.forEach(function(box) {
+            box.style.backgroundColor = Math.random() < 0.5 ? 'red' : 'green';
+        });
+    }
+
+    setInterval(changeBoxColors, 2000);
+});
